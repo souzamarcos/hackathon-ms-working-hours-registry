e4771f86d5555f56df79fe1f9ab236255032596c
Guilherme Oliveira <ghvoliveira@gmail.com>
2024-03-15 20:27:11 -0300

    feature - implantar padrão SAGA

--- listener/src/main/java/com/fiap/burger/listener/order/DefaultOrderMessageListener.java
+++ listener/src/main/java/com/fiap/burger/listener/order/DefaultOrderMessageListener.java
@@ -2,19 +2,24 @@ package com.fiap.burger.listener.order;
 
 import com.fiap.burger.controller.adapter.api.PaymentController;
 import com.fiap.burger.usecase.misc.exception.OrderMessageListenerException;
+import com.fiap.burger.usecase.misc.profiles.NotTest;
 import com.fiap.burger.usecase.misc.profiles.Production;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Production
+@NotTest
 @Component
 public class DefaultOrderMessageListener {
     @Autowired
     PaymentController paymentController;
 
-    @SqsListener("${cloud.aws.sqs.order-queue}")
+    @SqsListener(value = "${cloud.aws.sqs.order-queue}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @Transactional
     public void orderQueueListener(String message) {
         try {
             var dto = new Gson().fromJson(message, OrderMessageListenerDto.class);
